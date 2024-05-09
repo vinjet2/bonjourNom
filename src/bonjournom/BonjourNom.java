7278a6d3976741eabf9fff32054312fd8b1bd5cd
vinjet2 <vinjet2@gmail.com>
2024-05-09 19:11:24 -0400

    Changement de message

--- src/bonjournom/BonjourNom.java
+++ src/bonjournom/BonjourNom.java
@@ -16,7 +16,7 @@ public class BonjourNom {
      */
     public static void main(String[] args) {
         
-        System.out.println("Bonjour John Doe!");
+        System.out.println("Bonjour John Dodsdsde!");
     }
     
 }
